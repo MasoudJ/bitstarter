3fe49022fadef001d22a472d9ce6a0ff6928673b
Masoud Jalali <masoud.jalali.b@gmail.com>
2013-07-19 05:19:05 +0000

    Updated web.js 2

--- web.js
+++ web.js
@@ -3,10 +3,14 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World my 2!');
+    var fs = require('fs');
+    var content = fsreadFileSync('index.html', 'utf-8');
+    response.send(content);
+
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
